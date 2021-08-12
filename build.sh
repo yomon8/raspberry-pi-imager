292cd424841f70b6ea9feb1d0e14ddb2a3190ffd
yomon8 <sebanngou8@gmail.com>
2021-08-12 20:30:18 +0900

    fix

--- build.sh
+++ build.sh
@@ -5,15 +5,13 @@ cd ${SCRIPT_DIR}
 
 DOCKERFILE_DIR=./docker
 DOCKER_IMAGE_NAME=packer-raspberrypi-imager
-OUTPUT_IMAGE_DIR=raspberry-pi-image
 
 docker build -t ${DOCKER_IMAGE_NAME} ${DOCKERFILE_DIR}
 
-sudo rm -fR ./${OUTPUT_IMAGE_DIR}
 docker run \
   --rm \
   --privileged \
   -v ${PWD}:/build:ro \
   -v ${PWD}/packer_cache:/build/packer_cache \
-  -v ${PWD}/${OUTPUT_IMAGE_DIR}:/build/output-arm-image \
+  -v ${PWD}/output-arm-image:/build/output-arm-image \
   ${DOCKER_IMAGE_NAME} build /build/packer_raspberrypi.json
